7efddaf4dbcbe9990ab2c5cd4eba24a959b89d71
Scott Bevington <sabevington@lifesouth.org>
2016-03-04 14:05:13 -0500

    Work on validatedinput

--- components/ValidatedInput.js
+++ components/ValidatedInput.js
@@ -1,14 +1,34 @@
 import React, { Component, PropTypes } from 'react'
 import TextInput from './TextInput'
 
+export const STATUS = {
+  GOOD : "Good",
+  BAD : "Bad",
+  WARN: "Warn",
+  SCAN: "Scan",
+  EMPTY: "Empty"
+}
+
 export default class ValidatedInput extends TextInput {
   constructor(props) {
     super(props)
+//    this.setState({ status: checkFormat( props.value ) });
+  }
+
+  componentWillReceiveProps(nextProps) {
+  //  this.setState({ status: checkFormat( nextProps.value ) });
+  }
+
+  handleChange() {
+
+  }
+  checkFormat( value ) {
+      return STATUS.GOOD;
   }
 
   render() {
     return (
-      <TextInput className="StatusGood" id={this.props.id} value={this.props.value} {...this.props}/>
+      <TextInput id={this.props.id} value={this.props.value} {...this.props}/>
   ); }
 }
 
