77cff6c75b240b63c372f09302f09bba575c3828
Scott Bevington <sabevington@lifesouth.org>
2016-03-18 17:17:21 -0400

    Made the DIN page show only the passed din.

--- containers/DinPage.js
+++ containers/DinPage.js
@@ -3,57 +3,18 @@ import { connect } from 'react-redux'
 import merge from 'lodash/merge'
 import { loadDINs } from '../actions'
 import DIN from '../components/DIN'
-import PCode from '../components/PCode'
-import DINList from '../components/DINList'
-
-function loadData(props) {
-  props.loadDINs()
-}
 
 class DINPage extends Component {
   constructor(props) {
     super(props)
-    this.renderDIN = this.renderDIN.bind(this)
-  }
-
-  componentWillMount() {
-    loadData(this.props)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    loadData(nextProps)
-  }
-
-  renderDIN(din) {
-    return (
-      <div key={din.din+din.dinflag}>
-      <DIN din={din.din} dinflag={din.dinflag} />
-      <PCode pcode={din.pcode} />
-      </div>
-    )
   }
 
   render() {
-    const { dins } = this.props
-    console.log("DINsPage")
-    console.dir( this.props )
-    console.dir( dins )
+    const { din } = this.props.params
     return (
-      <div>
-        <DINList renderItem={this.renderDIN}
-              items={dins}
-              loadingLabel={`Loading DINs...`} />
-      </div>
+      <DIN din={din} />
     )
   }
 }
 
-function mapStateToProps(state, ownProps) {
-  const { entities: { dins } } = state
-  console.log("map")
-  return merge({}, ownProps, {dins: dins})
-}
-
-export default connect(mapStateToProps, {
-  loadDINs
-})(DINPage)
+export default DINPage
